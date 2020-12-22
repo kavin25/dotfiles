d2443428907ede862802995cb1ac9b58c37ec272
Kavin Desi Valli <kavinvalli@gmail.com>
2020-12-22 17:47:48 +0530

    Added VSCODE symlink on setup.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,3 @@
-echo "Checking OS Type"
-
-if [ "$(uname)" == "Darwin" ]; then
-    echo "Darwin here"
-    echo "Installing Homebrew"
-    # /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo "Git version $(git --version)"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "Linux here"
-    echo "Installing VS Code"
-    sudo snap install --classic-code
-fi
+echo "Hi from Ubuntu!"
+echo "Installing VS Code"
+sudo snap install --classic-code
--- setup.sh
+++ setup.sh
@@ -1,30 +1,30 @@
 echo "Backing up old files...\n"
 
 move() {
-		FILENAME=$1
-
-		echo "found $FILENAME"
-		mv $FILENAME "$FILENAME.bck" && echo "moved $FILENAME to $FILENAME.bck"
-		echo;
+    FILENAME=$1
+    
+    echo "found $FILENAME"
+    mv $FILENAME "$FILENAME.bck" && echo "moved $FILENAME to $FILENAME.bck"
+    echo;
 }
 
 backup() {
-	FILENAME=$1
-
-	[ -e $FILENAME ] && move $FILENAME
-	[ -h $FILENAME ] && move $FILENAME
+    FILENAME=$1
+    
+    [ -e $FILENAME ] && move $FILENAME
+    [ -h $FILENAME ] && move $FILENAME
 }
 
 symlinkhome() {
-	FILENAME=$1
-
-	ln -s ~/dotfiles/$FILENAME ~/$FILENAME
+    FILENAME=$1
+    
+    ln -s ~/dotfiles/$FILENAME ~/$FILENAME
 }
 
 symlinkconfig() {
-	FILENAME=$1
-
-	ln -s ~/dotfiles/$FILENAME ~/.config/$FILENAME
+    FILENAME=$1
+    
+    ln -s ~/dotfiles/$FILENAME ~/.config/$FILENAME
 }
 
 backup dotfiles
@@ -35,6 +35,8 @@ backup .config/vimb
 backup .emacs.d
 backup .qutebrowser
 backup .zprofile
+backup .config/Code/User/settings.json
+backup .config/Code/User/snippets
 
 echo "Cloning dotfiles repo...\n"
 git clone https://github.com/kavin25/dotfiles.git dotfiles
@@ -47,5 +49,7 @@ symlinkhome .qutebrowser
 symlinkhome .zprofile
 symlinkconfig nvim
 symlinkconfig vimb
+symlinkconfig Code/User/settings.json
+symlinkconfig Code/User/snippets
 
 echo "DONE!!!"
